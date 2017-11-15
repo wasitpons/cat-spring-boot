095b49c884589692ce186e83477d501f66f4e99d
Wasitpon-S <Wasitpon.s@hotmail.com>
2017-11-16 02:50:48 +0700

    Template

---
+++ src/main/java/com/wasitpon/project/CatProjectApplication.java
@@ -0,0 +1,12 @@
+package com.wasitpon.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatProjectApplication.class, args);
+	}
+}
